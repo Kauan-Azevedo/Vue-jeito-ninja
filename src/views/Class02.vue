e249f943aa025f973875e5f3c8801c7532567441
Kauan-Azevedo <kauanazevedo2010hotmail.com>
2022-11-07 13:55:14 -0300

    feat: class 02 added

---
+++ src/views/Class02.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container>
+        <h2>{{ title }}</h2>
+        <div>
+            <p>-model</p>
+            <p>v-if</p>
+            <p>v-else</p>
+            <p>v-show</p>
+        </div>
+        <!-- <div v-if v-else v-show>
+        </div> -->
+    </v-container>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            title: "Aula 02 - Diretivas - VueJS do jeito Ninja"
+        }
+    }
+}
+
+</script>
